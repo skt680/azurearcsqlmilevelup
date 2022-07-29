e7d6cecb24a298affd83f54e3f42bd5d41366d9d
Shaurabh Thapa <shaurath@microsoft.com>
2022-07-29 17:55:36 +0100

    v1-AKS-Deployment-Script

--- scripts/aks-deployment.ps1
+++ scripts/aks-deployment.ps1
@@ -8,11 +8,12 @@ $directconnectivity = Read-Host - prompt "Are you using for direct connectivity 
 
 
 # Default value start
-$rg_name="levelupaks_lab1"
-$StableVersion = "1.22.11"
+$rg_name="levelupaks_lab"
+#$StableVersion = "1.22.11"
+$StableVersion = ((az aks get-versions --location uksouth --query orchestrators | convertfrom-json) | where-object {$_.default -eq "True"}).orchestratorVersion
 #Need to automate with variable to catch the stable version for default value
 '''
-az aks get-versions --location  --$region  orchestrators –o table
+az aks get-versions --location  --$region  orchestrators -o table
 '''
 if($directconnectivity -eq "Yes")
 {
@@ -26,18 +27,49 @@ else{ $aksname = "aksindirect"
 # Default value End
 
 #az login 
-az account set –-subscription "$subscription"
+az account set --subscription "$subscription"
 
-$region
-$subscription
-$directconnectivity
-$aksname
+try
+{
+    Write-Host "Creating resource group $rg_name in region $region"
+    $run_command = az group create --name $rg_name --location $region
+
+}
+catch
+{
+    $errormessage = $_
+    
+    Write-Host "Script failed creating resource group with error :  "
+    throw $errormessage
+        
+}
+
+try
+{
+    Write-Host "Creating aks cluster with name $aksname with version $stableversion"
+    $run_command = az aks create -n $aksname -g $rg_name --generate-ssh-keys --node-vm-size Standard_D8s_v3 --node-count 2 --kubernetes-version $StableVersion --enable-cluster-autoscaler --min-count 1 --max-count 5
+    Write-Host "AKS cluster created"
+}
+catch
+{
+    $errormessage = $_
+    
+    Write-Host "Script failed creating aks cluster with error :  "
+    throw $errormessage
+        
+}
 
 
-az group create --name $rg_name --location $region
 
-
-az aks create -n $aksname -g $rg_name --generate-ssh-keys –-node-vm-size Standard_D8s_v3 --node-count 2 --kubernetes-version $StableVersion --enable-cluster-autoscaler --min-count 1 --max-count 5
-
-az aks get-credentials --resource-group $rg_name --name $aksname
+try
+{
+    Write-Host "Merging credintial for aks cluster $aksname"
+    $run_command = az aks get-credentials --resource-group $rg_name --name $aksname
+    write-host "Merge successfully"
+}
+catch
+{
+     $errormessage = $_
+     throw $errormessage
+}
 
